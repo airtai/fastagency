9e444845ba2604f2b4c9e1776be9c2d74910dd2b
Kumaran Rajendhiran <kumaran@airt.ai>
2024-07-26 13:49:47 +0000

    Optimize code

--- fastagency/db/base.py
+++ fastagency/db/base.py
@@ -1,11 +1,6 @@
 from contextlib import asynccontextmanager
-from typing import (
-    Any,
-    AsyncGenerator,
-    Dict,
-    Protocol,
-    runtime_checkable,
-)
+from os import environ
+from typing import Any, AsyncGenerator, Dict, Optional, Protocol, runtime_checkable
 
 from prisma import Prisma  # type: ignore[attr-defined]
 from prisma.actions import AuthTokenActions, ModelActions
@@ -14,14 +9,40 @@ from prisma.actions import AuthTokenActions, ModelActions
 @runtime_checkable
 class BaseProtocol(Protocol):
     @staticmethod
-    async def get_db_url() -> str: ...
+    async def get_db_url(env_var: str) -> str: ...
 
     @asynccontextmanager  # type: ignore[arg-type]
     async def get_db_connection(self) -> AsyncGenerator[Prisma, None]: ...
 
 
 @runtime_checkable
-class BaseBackendProtocol(BaseProtocol, Protocol):
+class BaseDBProtocol(Protocol):
+    ENV_VAR: str
+
+    @staticmethod
+    async def get_db_url(env_var: str) -> str:
+        db_url: Optional[str] = environ.get(env_var, None)
+        if not db_url:
+            raise ValueError(
+                f"No database URL provided nor set as environment variable '{env_var}'"
+            )
+        if "connect_timeout" not in db_url:
+            db_url += "?connect_timeout=60"
+        return db_url
+
+    @asynccontextmanager
+    async def get_db_connection(self) -> AsyncGenerator[Prisma, None]:
+        db_url = await self.get_db_url(self.ENV_VAR)
+        db = Prisma(datasource={"url": db_url})
+        await db.connect()
+        try:
+            yield db
+        finally:
+            await db.disconnect()
+
+
+@runtime_checkable
+class BaseBackendProtocol(BaseDBProtocol, Protocol):
     async def find_model_using_raw(self, model_uuid: str) -> Dict[str, Any]: ...
 
     @asynccontextmanager  # type: ignore[arg-type]
@@ -36,5 +57,5 @@ class BaseBackendProtocol(BaseProtocol, Protocol):
 
 
 @runtime_checkable
-class BaseFrontendProtocol(BaseProtocol, Protocol):
+class BaseFrontendProtocol(BaseDBProtocol, Protocol):
     async def get_user(self, user_uuid: str) -> Dict[str, Any]: ...
