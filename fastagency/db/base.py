b0d522a3b3fddae0bebca955fc15a7d2d0b0d4bc
Kumaran Rajendhiran <kumaran@airt.ai>
2024-07-29 08:17:52 +0000

    Remove unnecessary methods

--- fastagency/db/base.py
+++ fastagency/db/base.py
@@ -8,22 +8,10 @@ from typing import (
     runtime_checkable,
 )
 
-from prisma.actions import AuthTokenActions, ModelActions
-
 
 @runtime_checkable
 class BaseBackendProtocol(Protocol):
     async def find_model(self, model_uuid: str) -> Dict[str, Any]: ...
-
-    @asynccontextmanager  # type: ignore[arg-type]
-    async def get_model_connection(
-        self,
-    ) -> AsyncGenerator[ModelActions[Any], None]: ...
-
-    @asynccontextmanager  # type: ignore[arg-type]
-    async def get_authtoken_connection(
-        self,
-    ) -> AsyncGenerator[AuthTokenActions[Any], None]: ...
 
 
 @runtime_checkable
