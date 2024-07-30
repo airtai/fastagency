9cda7d27e279edd6931c14db82f1bd005edbe06b
Kumaran Rajendhiran <kumaran@airt.ai>
2024-07-30 12:06:33 +0000

    Add inmemory protocol implementation

---
+++ fastagency/db/inmemory.py
@@ -0,0 +1,146 @@
+import json
+from datetime import datetime
+from typing import Any, Dict, List, Optional, Union
+from uuid import UUID
+
+from fastapi import HTTPException
+
+from .base import BackendDBProtocol, FrontendDBProtocol
+
+
+class InMemoryBackendDB(BackendDBProtocol):
+    def __init__(self) -> None:
+        """In memory backend database."""
+        self._models: List[Dict[str, Any]] = []
+        self._auth_tokens: List[Dict[str, Any]] = []
+
+    async def create_model(
+        self,
+        model_uuid: str,
+        user_uuid: str,
+        type_name: str,
+        model_name: str,
+        json_str: str,
+    ) -> Dict[str, Any]:
+        model = {
+            "uuid": model_uuid,
+            "user_uuid": user_uuid,
+            "type_name": type_name,
+            "model_name": model_name,
+            "json_str": json.loads(json_str),
+            "created_at": datetime.now(),
+            "updated_at": datetime.now(),
+        }
+        self._models.append(model)
+        return model
+
+    async def find_model(self, model_uuid: Union[str, UUID]) -> Dict[str, Any]:
+        for model in self._models:
+            if model["uuid"] == str(model_uuid):
+                return model
+        raise HTTPException(
+            status_code=404, detail="Something went wrong. Please try again later."
+        )
+
+    async def find_many_model(
+        self, user_uuid: str, type_name: Optional[str] = None
+    ) -> List[Dict[str, Any]]:
+        return [model for model in self._models if model["user_uuid"] == user_uuid]
+
+    async def update_model(
+        self,
+        model_uuid: str,
+        user_uuid: str,
+        type_name: str,
+        model_name: str,
+        json_str: str,
+    ) -> Dict[str, Any]:
+        for model in self._models:
+            if model["uuid"] == model_uuid:
+                model["type_name"] = type_name
+                model["model_name"] = model_name
+                model["json_str"] = json.loads(json_str)
+                model["updated_at"] = datetime.now()
+                return model
+        raise HTTPException(status_code=404, detail="Model not found")
+
+    async def delete_model(self, model_uuid: str) -> Dict[str, Any]:
+        for model in self._models:
+            if model["uuid"] == model_uuid:
+                self._models.remove(model)
+                return model
+        raise HTTPException(status_code=404, detail="Model not found")
+
+    async def create_auth_token(
+        self,
+        auth_token_uuid: str,
+        name: str,
+        user_uuid: str,
+        deployment_uuid: str,
+        hashed_auth_token: str,
+        expiry: str,
+        expires_at: datetime,
+    ) -> Dict[str, Any]:
+        auth_token = {
+            "uuid": auth_token_uuid,
+            "name": name,
+            "user_uuid": user_uuid,
+            "deployment_uuid": deployment_uuid,
+            "hashed_auth_token": hashed_auth_token,
+            "expiry": expiry,
+            "expires_at": expires_at,
+            "created_at": datetime.now(),
+            "updated_at": datetime.now(),
+        }
+        self._auth_tokens.append(auth_token)
+        return auth_token
+
+    async def find_many_auth_token(
+        self, user_uuid: str, deployment_uuid: str
+    ) -> List[Dict[str, Any]]:
+        return [
+            auth_token
+            for auth_token in self._auth_tokens
+            if auth_token["user_uuid"] == user_uuid
+            and auth_token["deployment_uuid"] == deployment_uuid
+        ]
+
+    async def delete_auth_token(
+        self, auth_token_uuid: str, deployment_uuid: str, user_uuid: str
+    ) -> Dict[str, Any]:
+        for auth_token in self._auth_tokens:
+            if (
+                auth_token["uuid"] == auth_token_uuid
+                and auth_token["user_uuid"] == user_uuid
+                and auth_token["deployment_uuid"] == deployment_uuid
+            ):
+                self._auth_tokens.remove(auth_token)
+                return auth_token
+        raise HTTPException(status_code=404, detail="Auth token not found")
+
+
+class InMemoryFrontendDB(FrontendDBProtocol):
+    def __init__(self) -> None:
+        """In memory frontend database."""
+        self._users: List[Dict[str, Any]] = []
+
+    async def get_user(self, user_uuid: Union[int, str]) -> Any:
+        for user in self._users:
+            if user["uuid"] == str(user_uuid):
+                return user
+        raise HTTPException(status_code=404, detail=f"user_uuid {user_uuid} not found")
+
+    async def _create_user(
+        self, user_uuid: Union[int, str], email: str, username: str
+    ) -> str:
+        """Only to create user in testing."""
+        self._users.append(
+            {
+                "uuid": str(user_uuid),
+                "email": email,
+                "username": username,
+                "created_at": datetime.now(),
+                "updated_at": datetime.now(),
+            }
+        )
+        return str(user_uuid)
---
+++ tests/db/test_inmemory.py
@@ -0,0 +1,147 @@
+import random
+import uuid
+from datetime import datetime, timedelta
+from typing import Any, Dict
+
+import pytest
+
+import fastagency.db
+import fastagency.db.inmemory
+from fastagency.db.base import BackendDBProtocol, FrontendDBProtocol
+from fastagency.db.inmemory import InMemoryBackendDB, InMemoryFrontendDB
+from fastagency.models.llms.azure import AzureOAIAPIKey
+
+
+@pytest.mark.asyncio()
+class TestInMemoryFrontendDB:
+    async def test_set_default(self) -> None:
+        frontend_db = InMemoryFrontendDB()
+        with FrontendDBProtocol.set_default(frontend_db):
+            assert FrontendDBProtocol._default_db == frontend_db
+
+    async def test_db(self) -> None:
+        frontend_db = InMemoryFrontendDB()
+        with FrontendDBProtocol.set_default(frontend_db):
+            assert FrontendDBProtocol.db() == frontend_db
+
+    async def test_create_user_get_user(self) -> None:
+        frontend_db = InMemoryFrontendDB()
+
+        random_id = random.randint(1, 1_000_000)
+        generated_uuid = str(uuid.uuid4())
+        email = f"user{random_id}@airt.ai"
+        username = f"user{random_id}"
+
+        user_uuid = await frontend_db._create_user(generated_uuid, email, username)
+        assert user_uuid == generated_uuid
+
+        user = await frontend_db.get_user(user_uuid)
+        assert user["uuid"] == user_uuid
+        assert user["email"] == email
+        assert user["username"] == username
+
+
+@pytest.mark.db()
+@pytest.mark.asyncio()
+class TestInMemoryBackendDB:
+    async def test_set_default(self) -> None:
+        backend_db = InMemoryBackendDB()
+        with BackendDBProtocol.set_default(backend_db):
+            assert BackendDBProtocol._default_db == backend_db
+
+    async def test_db(self) -> None:
+        backend_db = InMemoryBackendDB()
+        with BackendDBProtocol.set_default(backend_db):
+            assert BackendDBProtocol.db() == backend_db
+
+    async def test_model_CRUD(self) -> None:  # noqa: N802
+        # Setup
+        frontend_db = InMemoryFrontendDB()
+        backend_db = InMemoryBackendDB()
+        random_id = random.randint(1, 1_000_000)
+        user_uuid = await frontend_db._create_user(
+            str(uuid.uuid4()), f"user{random_id}@airt.ai", f"user{random_id}"
+        )
+        model_uuid = str(uuid.uuid4())
+        azure_oai_api_key = AzureOAIAPIKey(api_key="whatever", name="who cares?")
+
+        # Tests
+        model = await backend_db.create_model(
+            user_uuid=user_uuid,
+            model_uuid=model_uuid,
+            type_name="secret",
+            model_name="AzureOAIAPIKey",
+            json_str=azure_oai_api_key.model_dump_json(),
+        )
+        assert model["uuid"] == model_uuid
+        assert model["user_uuid"] == user_uuid
+        assert model["type_name"] == "secret"
+        assert model["model_name"] == "AzureOAIAPIKey"
+        assert model["json_str"] == azure_oai_api_key.model_dump()
+
+        found_model = await backend_db.find_model(model_uuid)
+        assert found_model["uuid"] == model_uuid
+
+        many_model = await backend_db.find_many_model(user_uuid)
+        assert len(many_model) == 1
+        assert many_model[0]["uuid"] == model_uuid
+
+        updated_model = await backend_db.update_model(
+            model_uuid=model_uuid,
+            user_uuid=user_uuid,
+            type_name="secret",
+            model_name="AzureOAIAPIKey2",
+            json_str=azure_oai_api_key.model_dump_json(),
+        )
+        assert updated_model["uuid"] == model_uuid
+        assert updated_model["model_name"] == "AzureOAIAPIKey2"
+
+        deleted_model = await backend_db.delete_model(model_uuid)
+        assert deleted_model["uuid"] == model_uuid
+
+    async def test_auth_token_CRUD(self, monkeypatch: pytest.MonkeyPatch) -> None:  # noqa: N802
+        # Setup
+        frontend_db = InMemoryFrontendDB()
+        backend_db = InMemoryBackendDB()
+        random_id = random.randint(1, 1_000_000)
+        user_uuid = await frontend_db._create_user(
+            str(uuid.uuid4()), f"user{random_id}@airt.ai", f"user{random_id}"
+        )
+        deployment_uuid = str(uuid.uuid4())
+        auth_token_uuid = str(uuid.uuid4())
+
+        async def mock_find_model(*args: Any, **kwargs: Any) -> Dict[str, str]:
+            return {
+                "user_uuid": user_uuid,
+                "uuid": deployment_uuid,
+            }
+
+        monkeypatch.setattr(
+            fastagency.db.inmemory.InMemoryBackendDB,
+            "find_model",
+            mock_find_model,
+        )
+
+        # Tests
+        auth_token = await backend_db.create_auth_token(
+            auth_token_uuid=auth_token_uuid,
+            name="Test token",
+            user_uuid=user_uuid,
+            deployment_uuid=deployment_uuid,
+            hashed_auth_token="whatever",
+            expiry="99d",
+            expires_at=datetime.utcnow() + timedelta(days=99),
+        )
+        assert auth_token["uuid"] == auth_token_uuid
+        assert auth_token["name"] == "Test token"
+
+        many_auth_token = await backend_db.find_many_auth_token(
+            user_uuid, deployment_uuid
+        )
+        assert len(many_auth_token) == 1
+        assert many_auth_token[0]["uuid"] == auth_token_uuid
+
+        deleted_auth_token = await backend_db.delete_auth_token(
+            auth_token_uuid, deployment_uuid, user_uuid
+        )
+        assert deleted_auth_token["uuid"] == auth_token_uuid
