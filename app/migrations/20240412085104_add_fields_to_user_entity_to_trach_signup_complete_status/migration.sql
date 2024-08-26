cb48fe11cf705c749608aaee1e57a960c08fae87
Kumaran Rajendhiran <kumaran@airt.ai>
2024-08-26 08:15:59 +0000

    Delete wasp directory and retain wasp shema prisma

--- app/migrations/20240412085104_add_fields_to_user_entity_to_trach_signup_complete_status/migration.sql
+++
@@ -1,4 +0,0 @@
--- AlterTable
-ALTER TABLE "User" ADD COLUMN     "hasAcceptedTos" BOOLEAN NOT NULL DEFAULT false,
-ADD COLUMN     "hasSubscribedToMarketingEmails" BOOLEAN NOT NULL DEFAULT false,
-ADD COLUMN     "isSignUpComplete" BOOLEAN NOT NULL DEFAULT false;
