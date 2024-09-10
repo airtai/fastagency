956ac676b8860cbbc0dbec1a01491f58df2e81dc
Davor Runje <davor@airt.ai>
2024-09-10 20:17:56 +0000

    test fixed

--- tests/examples/test_cli.py
+++ tests/examples/test_cli.py
@@ -9,15 +9,19 @@ runner = CliRunner()
 INPUT_MESSAGE = "Who is Leonardo da Vinci?"
 
 
-@pytest.mark.skip("Not working")
 @pytest.mark.openai
 @pytest.mark.parametrize(
-    ("input_message", "path"), [("Who is Leonardo da Vinci?", "examples/cli/main.py ")]
+    ("input_message", "path"),
+    [
+        ("Who is Leonardo da Vinci?", "examples/cli/main.py"),
+    ],
 )
 def test_cli(monkeypatch: pytest.MonkeyPatch, input_message: str, path: str) -> None:
     monkeypatch.setattr("builtins.input", InputMock([input_message]))
 
     result = runner.invoke(app, ["run", path], color=True)
+
+    print(result.stdout)  # noqa: T201
 
     assert INPUT_MESSAGE in result.stdout
     assert "workflow_completed" in result.stdout
