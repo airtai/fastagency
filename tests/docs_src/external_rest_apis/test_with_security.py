65c3dee1aa78643758a59937722c7d1d32ae9d7a
Kumaran Rajendhiran <kumaran@airt.ai>
2024-09-10 10:36:44 +0000

    Add cli test for docs_src

--- tests/docs_src/external_rest_apis/test_with_security.py
+++ tests/docs_src/external_rest_apis/test_with_security.py
@@ -1,21 +1,37 @@
 import pytest
+from typer.testing import CliRunner
 
+from fastagency.cli import app
 from fastagency.core.io.console import ConsoleIO
 from tests.conftest import InputMock
 
+runner = CliRunner()
+
+INPUT_MESSAGE = "Get me hourly forecast for Zagreb city"
+
 
 @pytest.mark.openai
-def test_without_security(monkeypatch: pytest.MonkeyPatch) -> None:
+def test_wf_with_security(monkeypatch: pytest.MonkeyPatch) -> None:
     from docs.docs_src.tutorial.external_rest_apis.security import wf
 
     monkeypatch.setattr("builtins.input", InputMock([""] * 5))
 
-    initial_message = "Get me hourly forecast for Zagreb city"
     result = wf.run(
         name="simple_weather_with_security",
         session_id="session_id",
         io=ConsoleIO(),
-        initial_message=initial_message,
+        initial_message=INPUT_MESSAGE,
     )
 
     assert result is not None
+
+
+@pytest.mark.openai
+def test_cli_with_security(monkeypatch: pytest.MonkeyPatch) -> None:
+    monkeypatch.setattr("builtins.input", InputMock([INPUT_MESSAGE]))
+
+    result = runner.invoke(
+        app, ["run", "docs/docs_src/tutorial/external_rest_apis/security.py"]
+    )
+    assert INPUT_MESSAGE in result.stdout
+    assert "get_hourly_weather_hourly_get" in result.stdout
--- tests/docs_src/external_rest_apis/test_without_security.py
+++ tests/docs_src/external_rest_apis/test_without_security.py
@@ -1,21 +1,37 @@
 import pytest
+from typer.testing import CliRunner
 
+from fastagency.cli import app
 from fastagency.core.io.console import ConsoleIO
 from tests.conftest import InputMock
 
+runner = CliRunner()
+
+INPUT_MESSAGE = "Get me daily forecast for Zagreb city"
+
 
 @pytest.mark.openai
-def test_without_security(monkeypatch: pytest.MonkeyPatch) -> None:
+def test_wf_without_security(monkeypatch: pytest.MonkeyPatch) -> None:
     from docs.docs_src.tutorial.external_rest_apis.main import wf
 
     monkeypatch.setattr("builtins.input", InputMock([""] * 5))
 
-    initial_message = "Get me daily forecast for Zagreb city"
     result = wf.run(
         name="simple_weather",
         session_id="session_id",
         io=ConsoleIO(),
-        initial_message=initial_message,
+        initial_message=INPUT_MESSAGE,
     )
 
     assert result is not None
+
+
+@pytest.mark.openai
+def test_cli_without_security(monkeypatch: pytest.MonkeyPatch) -> None:
+    monkeypatch.setattr("builtins.input", InputMock([INPUT_MESSAGE]))
+
+    result = runner.invoke(
+        app, ["run", "docs/docs_src/tutorial/external_rest_apis/main.py"]
+    )
+    assert INPUT_MESSAGE in result.stdout
+    assert "get_daily_weather_daily_get" in result.stdout
