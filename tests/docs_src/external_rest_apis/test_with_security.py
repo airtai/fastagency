a5795091f99f8dda48f28e12b4238509ea1d8e0e
Kumaran Rajendhiran <kumaran@airt.ai>
2024-09-10 11:10:38 +0000

    Disable workflow tests as experiment

--- tests/docs_src/external_rest_apis/test_with_security.py
+++ tests/docs_src/external_rest_apis/test_with_security.py
@@ -2,7 +2,8 @@ import pytest
 from typer.testing import CliRunner
 
 from fastagency.cli import app
-from fastagency.core.io.console import ConsoleIO
+
+# from fastagency.core.io.console import ConsoleIO
 from tests.conftest import InputMock
 
 runner = CliRunner()
@@ -10,20 +11,20 @@ runner = CliRunner()
 INPUT_MESSAGE = "Get me hourly forecast for Zagreb city"
 
 
-@pytest.mark.openai
-def test_wf_with_security(monkeypatch: pytest.MonkeyPatch) -> None:
-    from docs.docs_src.tutorial.external_rest_apis.security import wf
+# @pytest.mark.openai
+# def test_wf_with_security(monkeypatch: pytest.MonkeyPatch) -> None:
+#     from docs.docs_src.tutorial.external_rest_apis.security import wf
 
-    monkeypatch.setattr("builtins.input", InputMock([""] * 5))
+#     monkeypatch.setattr("builtins.input", InputMock([""] * 5))
 
-    result = wf.run(
-        name="simple_weather_with_security",
-        session_id="session_id",
-        io=ConsoleIO(),
-        initial_message=INPUT_MESSAGE,
-    )
+#     result = wf.run(
+#         name="simple_weather_with_security",
+#         session_id="session_id",
+#         io=ConsoleIO(),
+#         initial_message=INPUT_MESSAGE,
+#     )
 
-    assert result is not None
+#     assert result is not None
 
 
 @pytest.mark.openai
--- tests/docs_src/external_rest_apis/test_without_security.py
+++ tests/docs_src/external_rest_apis/test_without_security.py
@@ -2,7 +2,8 @@ import pytest
 from typer.testing import CliRunner
 
 from fastagency.cli import app
-from fastagency.core.io.console import ConsoleIO
+
+# from fastagency.core.io.console import ConsoleIO
 from tests.conftest import InputMock
 
 runner = CliRunner()
@@ -10,20 +11,20 @@ runner = CliRunner()
 INPUT_MESSAGE = "Get me daily forecast for Zagreb city"
 
 
-@pytest.mark.openai
-def test_wf_without_security(monkeypatch: pytest.MonkeyPatch) -> None:
-    from docs.docs_src.tutorial.external_rest_apis.main import wf
+# @pytest.mark.openai
+# def test_wf_without_security(monkeypatch: pytest.MonkeyPatch) -> None:
+#     from docs.docs_src.tutorial.external_rest_apis.main import wf
 
-    monkeypatch.setattr("builtins.input", InputMock([""] * 5))
+#     monkeypatch.setattr("builtins.input", InputMock([""] * 5))
 
-    result = wf.run(
-        name="simple_weather",
-        session_id="session_id",
-        io=ConsoleIO(),
-        initial_message=INPUT_MESSAGE,
-    )
+#     result = wf.run(
+#         name="simple_weather",
+#         session_id="session_id",
+#         io=ConsoleIO(),
+#         initial_message=INPUT_MESSAGE,
+#     )
 
-    assert result is not None
+#     assert result is not None
 
 
 @pytest.mark.openai
