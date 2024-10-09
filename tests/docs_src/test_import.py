8918857199e86ddd76449d0c969ad9bdb3d1d55d
Davor Runje <davor@airt.ai>
2024-10-09 21:27:11 +0000

    test fix in 3.9

--- tests/docs_src/test_import.py
+++ tests/docs_src/test_import.py
@@ -27,11 +27,25 @@ def test_list_submodules() -> None:
 @pytest.mark.parametrize("module", list_submodules(module_name, include_path=root_path))
 def test_submodules(module: str) -> None:
     with add_to_sys_path(root_path):
-        if "mesop" in module and sys.version_info < (3, 10):
-            with pytest.raises(
-                FastAgencyCLIPythonVersionError,
-                match="Mesop requires Python 3.10 or higher",
+        if sys.version_info < (3, 10):
+            if module == "docs_src.user_guide.ui.mesop.main_mesop":
+                with pytest.raises(
+                    ModuleNotFoundError,
+                    match="No module named 'mesop'",
+                ):
+                    importlib.import_module(module)
+                return
+            elif (
+                module.startswith("docs_src.user_guide.ui.mesop")
+                or module == "docs_src.tutorial.giphy"
             ):
-                importlib.import_module(module)
-            return
+                pass
+            elif ("mesop" in module) or ("giphy" in module):
+                with pytest.raises(
+                    FastAgencyCLIPythonVersionError,
+                    match="Mesop requires Python 3.10 or higher",
+                ):
+                    importlib.import_module(module)
+                return
+
         importlib.import_module(module)
