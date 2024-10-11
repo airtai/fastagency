f5a116e23ec6b6a5c37949ef3122430e7aca9354
Tvrtko Sternak <sternakt@gmail.com>
2024-10-11 09:19:08 +0000

    Add schema test scenario

--- tests/api/openapi/templates/test_fastapi_codegen_template.py
+++ tests/api/openapi/templates/test_fastapi_codegen_template.py
@@ -2,38 +2,24 @@ import importlib
 import sys
 import tempfile
 from pathlib import Path
-from typing import Any, Union
 
-from _pytest.monkeypatch import MonkeyPatch
+import pytest
 from fastapi_code_generator.__main__ import generate_code
 
-OPENAPI_FILE_PATH = (Path(__file__).parent / "openapi.json").resolve()
+OPENAPI_FILE_PATHS = Path(__file__).parent.glob("*.json")
 TEMPLATE_DIR = Path(__file__).parents[4] / "templates"
 
-assert OPENAPI_FILE_PATH.exists(), OPENAPI_FILE_PATH
 assert TEMPLATE_DIR.exists(), TEMPLATE_DIR
 
 
-class MockResponse:
-    def __init__(
-        self, json_data: Union[list[dict[str, Any]], dict[str, Any]], status_code: int
-    ) -> None:
-        """Mock response object for requests."""
-        self.json_data = json_data
-        self.status_code = status_code
-
-    def json(self) -> Union[list[dict[str, Any]], dict[str, Any]]:
-        """Return the json data."""
-        return self.json_data
-
-
-def test_fastapi_codegen_template(monkeypatch: MonkeyPatch) -> None:
+@pytest.mark.parametrize("openapi_file_path", OPENAPI_FILE_PATHS)
+def test_fastapi_codegen_template(openapi_file_path: Path) -> None:
     with tempfile.TemporaryDirectory() as temp_dir:
         td = Path(temp_dir)
 
         generate_code(
-            input_name=OPENAPI_FILE_PATH.name,
-            input_text=OPENAPI_FILE_PATH.read_text(),
+            input_name=openapi_file_path.name,
+            input_text=openapi_file_path.read_text(),
             encoding="utf-8",
             output_dir=td,
             template_dir=TEMPLATE_DIR,
