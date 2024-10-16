d501a04c38aa15f9a43297b16e47bed3fd8ace5f
Robert Jambrecic <robert@airt.ai>
2024-10-16 12:28:30 +0000

    wip

--- docs/docs_src/tutorial/whatsapp/main.py
+++ docs/docs_src/tutorial/whatsapp/main.py
@@ -6,7 +6,7 @@ import os
 
 # from fastagency import UI, FastAgency
 from fastagency.api.openapi.client import OpenAPI
-from fastagency.api.openapi.security import APIKeyQuery
+from fastagency.api.openapi.security import APIKeyHeader
 # from fastagency.runtimes.autogen.autogen import AutoGenWorkflows
 # from fastagency.ui.mesop import MesopUI
 
@@ -23,14 +23,8 @@ llm_config = {
 with open("examples/openapi/whatsapp_openapi.json") as f:
     openapi_json = f.read()
 
-infobip_base_url = os.getenv("INFOBIP_BASE_URL", "")
-servers=[{"url": infobip_base_url, "description": "API base URL"}]
-try:
-    whatsapp_api = OpenAPI(
-        title="WhatsApp API",
-        servers=servers,
-    ).create(openapi_json=openapi_json)
-    whatsapp_api_key = os.getenv("WHATSAPP_API_KEY", "")
-    whatsapp_api.set_security_params(APIKeyQuery.Parameters(value=whatsapp_api_key))
-except Exception as e:
-    print(f"Error creating WhatsApp API: {e}")
+# infobip_base_url = os.getenv("INFOBIP_BASE_URL", "")
+whatsapp_api_key = os.getenv("WHATSAPP_API_KEY", "")
+
+whatsapp_api = OpenAPI.create(openapi_json=openapi_json)
+whatsapp_api.set_security_params(APIKeyHeader.Parameters(value=whatsapp_api_key))
