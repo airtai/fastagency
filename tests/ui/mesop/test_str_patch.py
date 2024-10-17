4ee68ce53c6208bb22dff484717b53b89fedcb35
Kumaran Rajendhiran <kumaran@airt.ai>
2024-10-17 11:28:05 +0000

    Run mesop tests only for python 3.10 and above

--- tests/ui/mesop/test_str_patch.py
+++ tests/ui/mesop/test_str_patch.py
@@ -12,40 +12,43 @@ from fastagency.ui.mesop.timer import (
 )
 
 
-@pytest.mark.parametrize(
-    "value, startswith, expected",  # noqa: PT006
-    [
-        (MEL_WEB_COMPONENT_PATH, "/", True),
-        (MEL_WEB_COMPONENT_PATH, "\\", False),
-        (WINDOWS_MEL_WEB_COMPONENT_PATH, "/", False),
-        (WINDOWS_MEL_WEB_COMPONENT_PATH, "\\", True),
-        ("random string", "r", True),
-        ("random string", "/", False),
-    ],
+@pytest.mark.skipif(
+    sys.version_info < (3, 10), reason="Mesop is not supported in Python 3.9"
 )
-def test_MyStr_startswith(value: str, startswith: str, expected: bool) -> None:  # noqa: N802
-    assert MyStr(value).startswith(startswith) == expected
+class TestStrOSPatch:
+    @pytest.mark.parametrize(
+        "value, startswith, expected",  # noqa: PT006
+        [
+            (MEL_WEB_COMPONENT_PATH, "/", True),
+            (MEL_WEB_COMPONENT_PATH, "\\", False),
+            (WINDOWS_MEL_WEB_COMPONENT_PATH, "/", False),
+            (WINDOWS_MEL_WEB_COMPONENT_PATH, "\\", True),
+            ("random string", "r", True),
+            ("random string", "/", False),
+        ],
+    )
+    def test_MyStr_startswith(  # noqa: N802
+        self, value: str, startswith: str, expected: bool
+    ) -> None:
+        assert MyStr(value).startswith(startswith) == expected
 
+    @pytest.mark.skipif(sys.platform != "win32", reason="Test only runs on Windows")
+    def test_MyStr_startswith_windows(self) -> None:  # noqa: N802
+        assert MyStr(WINDOWS_MEL_WEB_COMPONENT_PATH).startswith("/")
 
-@pytest.mark.skipif(sys.platform != "win32", reason="Test only runs on Windows")
-def test_MyStr_startswith_windows() -> None:  # noqa: N802
-    assert MyStr(WINDOWS_MEL_WEB_COMPONENT_PATH).startswith("/")
+    @pytest.mark.parametrize(
+        "value, expected",  # noqa: PT006
+        [
+            (MEL_WEB_COMPONENT_PATH, False),
+            (WINDOWS_MEL_WEB_COMPONENT_PATH, True),
+            ("random string", False),
+        ],
+    )
+    def test_os_path_normpath_patch(self, value: str, expected: bool) -> None:
+        actual = os_path_normpath_patch(value)
+        assert isinstance(actual, MyStr) == expected
 
-
-@pytest.mark.parametrize(
-    "value, expected",  # noqa: PT006
-    [
-        (MEL_WEB_COMPONENT_PATH, False),
-        (WINDOWS_MEL_WEB_COMPONENT_PATH, True),
-        ("random string", False),
-    ],
-)
-def test_os_path_normpath_patch(value: str, expected: bool) -> None:
-    actual = os_path_normpath_patch(value)
-    assert isinstance(actual, MyStr) == expected
-
-
-def test_patch_os_and_str() -> None:
-    with patch_os_and_str():
-        assert os.path.normpath is os_path_normpath_patch
-    assert os.path.normpath is not os_path_normpath_patch
+    def test_patch_os_and_str(self) -> None:
+        with patch_os_and_str():
+            assert os.path.normpath is os_path_normpath_patch
+        assert os.path.normpath is not os_path_normpath_patch
