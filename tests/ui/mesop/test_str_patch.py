0ba5602e502e531176c87dba7089f5adc96f1684
Kumaran Rajendhiran <kumaran@airt.ai>
2024-10-17 11:36:49 +0000

    Move imports intests

--- tests/ui/mesop/test_str_patch.py
+++ tests/ui/mesop/test_str_patch.py
@@ -3,19 +3,16 @@ import sys
 
 import pytest
 
-from fastagency.ui.mesop.timer import (
-    MEL_WEB_COMPONENT_PATH,
-    WINDOWS_MEL_WEB_COMPONENT_PATH,
-    MyStr,
-    os_path_normpath_patch,
-    patch_os_and_str,
-)
-
 
 @pytest.mark.skipif(
     sys.version_info < (3, 10), reason="Mesop is not supported in Python 3.9"
 )
 class TestStrOSPatch:
+    from fastagency.ui.mesop.timer import (
+        MEL_WEB_COMPONENT_PATH,
+        WINDOWS_MEL_WEB_COMPONENT_PATH,
+    )
+
     @pytest.mark.parametrize(
         "value, startswith, expected",  # noqa: PT006
         [
@@ -30,10 +27,19 @@ class TestStrOSPatch:
     def test_MyStr_startswith(  # noqa: N802
         self, value: str, startswith: str, expected: bool
     ) -> None:
+        from fastagency.ui.mesop.timer import (
+            MyStr,
+        )
+
         assert MyStr(value).startswith(startswith) == expected
 
     @pytest.mark.skipif(sys.platform != "win32", reason="Test only runs on Windows")
     def test_MyStr_startswith_windows(self) -> None:  # noqa: N802
+        from fastagency.ui.mesop.timer import (
+            WINDOWS_MEL_WEB_COMPONENT_PATH,
+            MyStr,
+        )
+
         assert MyStr(WINDOWS_MEL_WEB_COMPONENT_PATH).startswith("/")
 
     @pytest.mark.parametrize(
@@ -45,10 +51,20 @@ class TestStrOSPatch:
         ],
     )
     def test_os_path_normpath_patch(self, value: str, expected: bool) -> None:
+        from fastagency.ui.mesop.timer import (
+            MyStr,
+            os_path_normpath_patch,
+        )
+
         actual = os_path_normpath_patch(value)
         assert isinstance(actual, MyStr) == expected
 
     def test_patch_os_and_str(self) -> None:
+        from fastagency.ui.mesop.timer import (
+            os_path_normpath_patch,
+            patch_os_and_str,
+        )
+
         with patch_os_and_str():
             assert os.path.normpath is os_path_normpath_patch
         assert os.path.normpath is not os_path_normpath_patch
