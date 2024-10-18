9cf43e974a17dce8d017492ab360cd815b44abe6
Robert Jambrecic <robert@airt.ai>
2024-10-18 08:35:16 +0000

    wip

--- docs/docs_src/tutorial/whatsapp/main.py
+++ docs/docs_src/tutorial/whatsapp/main.py
@@ -20,43 +20,44 @@ llm_config = {
     "temperature": 0.8,
 }
 
-with (Path(__file__).parent / "../../../../examples/openapi/whatsapp_openapi.json").open() as f:
+whatsapp_openapi_path = Path(__file__).parent / "../../../../examples/openapi/whatsapp_openapi.json"
+with whatsapp_openapi_path.open() as f:
     openapi_json = f.read()
 
-# infobip_base_url = os.getenv("INFOBIP_BASE_URL", "")
-whatsapp_api_key = os.getenv("WHATSAPP_API_KEY", "")
-
 whatsapp_api = OpenAPI.create(
-    openapi_json=openapi_json
+    openapi_json=openapi_json,
 )
+
+whatsapp_api_key = "App " # pragma: allowlist secret
+whatsapp_api_key += os.getenv("WHATSAPP_API_KEY", "")
 whatsapp_api.set_security_params(APIKeyHeader.Parameters(value=whatsapp_api_key))
-
-
-def is_termination_msg(msg: dict[str, Any]) -> bool:
-    return msg["content"] is not None and "TERMINATE" in msg["content"]
-
 
 wf = AutoGenWorkflows()
 
+@wf.register(name="whatsapp", description="WhatsApp chat")
+def whatsapp_workflow(ui: UI, params: dict[str, Any]) -> str:
+    def is_termination_msg(msg: dict[str, Any]) -> bool:
+        return msg["content"] is not None and "TERMINATE" in msg["content"]
 
-@wf.register(name="giphy_and_websurfer", description="Giphy and Websurfer chat")
-def giphy_workflow_with_security(ui: UI, params: dict[str, Any]) -> str:
     user_proxy = UserProxyAgent(
         name="User_Proxy",
         human_input_mode="NEVER",
         is_termination_msg=is_termination_msg,
     )
 
-    WHATSAPP_SYSTEM_MESSAGE = """Msg Body should look like this:
+    WHATSAPP_SYSTEM_MESSAGE = """Msg Body must use the following format:
 {
-    "from": "senderNumber",
-    "to": "recipientNumber",
-    "messageId": "test-message-random-id",
+    "from": "447860099299",
+    "to": "receiverNumber",
+    "messageId": "test-message-randomInt",
     "content": {
-        "text":
+        "text": "message"
     },
     "callbackData": "Callback data"
-}"""
+}
+
+"from" number is always the same.
+"""
     whatsapp_agent = ConversableAgent(
         name="Whatsapp_Agent",
         system_message=WHATSAPP_SYSTEM_MESSAGE,
@@ -74,7 +75,8 @@ def giphy_workflow_with_security(ui: UI, params: dict[str, Any]) -> str:
     initial_message = ui.text_input(
         sender="Workflow",
         recipient="User",
-        prompt="I can help you send a message to a WhatsApp number. What message would you like to send?",
+        prompt="""I can help you send a message to your WhatsApp.
+What is your number and which message would you like to send?""",
     )
 
     chat_result = user_proxy.initiate_chat(
@@ -87,4 +89,4 @@ def giphy_workflow_with_security(ui: UI, params: dict[str, Any]) -> str:
     return chat_result.summary  # type: ignore[no-any-return]
 
 
-app = FastAgency(provider=wf, ui=ConsoleUI(), title="Giphy and Websurfer chat")
+app = FastAgency(provider=wf, ui=ConsoleUI(), title="WhatsApp chat")
