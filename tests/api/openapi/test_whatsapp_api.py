56313753710fcfe795406ea08721de0a5be19e6b
Robert Jambrecic <robert@airt.ai>
2024-10-30 07:56:58 +0000

    wip

--- tests/api/openapi/test_whatsapp_api.py
+++ tests/api/openapi/test_whatsapp_api.py
@@ -81,3 +81,76 @@ def test_real_whatsapp_end2end(
             "callbackData": "Callback data",
         },
     )
+
+
+@patch("fastagency.api.openapi.client.requests.post")
+def test_real_whatsapp_end2end_problematic(
+    mock_post: MagicMock,
+) -> None:
+    mock_response = MagicMock()
+    mock_response.status_code = 200
+    mock_response.json.return_value = {"status": "success"}
+    mock_post.return_value = mock_response
+
+    file_name = "whatsapp_simple.json"
+
+    file_path = (
+        Path(__file__).parent.parent.parent.parent / f"examples/openapi/{file_name}"
+    )
+
+    with file_path.open(encoding="utf-8") as file:
+        openapi_json = file.read()
+
+    api = OpenAPI.create(
+        openapi_json=openapi_json,
+        client_source_path=".",    
+    )
+
+    assert isinstance(api, OpenAPI)
+
+    user_proxy = UserProxyAgent(
+        name="user_proxy",
+        human_input_mode="NEVER",
+        code_execution_config=False,
+    )
+
+
+    functions = ["send_whatsapp_text_message"]
+    api._register_for_execution(user_proxy, functions=functions)
+    
+    assert tuple(user_proxy._function_map.keys()) == (
+        "send_whatsapp_text_message",
+    )
+
+    send_whatsapp_text_message = user_proxy._function_map[
+        "send_whatsapp_text_message"
+    ]
+
+    send_whatsapp_text_message(
+        **{
+            "body": {
+                "from": "447860099299",
+                "to": "38591152131",
+                "messageId": "test-message-123",
+                "content": {"text": "Hello, World!"},
+                "callbackData": "Callback data",
+            }
+        }
+    )
+
+    mock_post.assert_called_once()
+
+    mock_post.assert_called_once_with(
+        "https://api.infobip.com/whatsapp/1/message/text",
+        params={},
+        headers={
+            "Content-Type": "application/json",
+        },
+        json={
+            "from": "447860099299",
+            "to": "38591152131",
+            "messageId": "test-message-123",
+            "content": {"text": "Hello, World!"},
+            "callbackData": "Callback data",
+        },
+    )
