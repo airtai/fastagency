d70bb91f190f4de6919f22276a49e57b3d7120fd
Davor Runje <davor@airt.ai>
2024-11-06 20:51:12 +0000

    šre-commit fixes

--- scripts/build-docs.sh
+++ scripts/build-docs.sh
@@ -9,11 +9,11 @@ set -x
 cd docs/docs_src/getting_started && \
     rm -rf fastapi mesop nats_n_fastapi; \
     cookiecutter -f -o mesop --no-input https://github.com/airtai/cookiecutter-fastagency.git app_type=mesop && \
-    cd mesop && tree my_fastagency_app > folder_structure.txt && cd .. && \
+    cd mesop && tree --noreport --dirsfirst my_fastagency_app > folder_structure.txt && cd .. && \
     cookiecutter -f -o fastapi --no-input https://github.com/airtai/cookiecutter-fastagency.git app_type=fastapi+mesop && \
-    cd fastapi && tree my_fastagency_app > folder_structure.txt && cd .. && \
+    cd fastapi && tree --noreport --dirsfirst my_fastagency_app > folder_structure.txt && cd .. && \
     cookiecutter -f -o nats_n_fastapi --no-input https://github.com/airtai/cookiecutter-fastagency.git app_type=nats+fastapi+mesop && \
-    cd nats_n_fastapi && tree my_fastagency_app > folder_structure.txt && cd .. && \
+    cd nats_n_fastapi && tree --noreport --dirsfirst my_fastagency_app > folder_structure.txt && cd .. && \
     cd ../../..
 
 
