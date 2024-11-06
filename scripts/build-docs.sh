c6346cf13070bca2ef322dd29c893ef4336a3fca
Davor Runje <davor@airt.ai>
2024-11-06 15:17:30 +0000

    docs polising

--- scripts/build-docs.sh
+++ scripts/build-docs.sh
@@ -8,11 +8,11 @@ set -x
 # build docs/docs_src/getting_started
 cd docs/docs_src/getting_started && \
     cookiecutter -f -o mesop --no-input https://github.com/airtai/cookiecutter-fastagency.git app_type=mesop && \
-    cd mesop && tree my_fastagency_app | sed 's/^/        /' > folder_structure.txt && cd .. && \
+    cd mesop && tree my_fastagency_app > folder_structure.txt && cd .. && \
     cookiecutter -f -o fastapi --no-input https://github.com/airtai/cookiecutter-fastagency.git app_type=fastapi+mesop && \
-    cd fastapi && tree my_fastagency_app | sed 's/^/        /' > folder_structure.txt && cd .. && \
+    cd fastapi && tree my_fastagency_app > folder_structure.txt && cd .. && \
     cookiecutter -f -o nats_n_fastapi --no-input https://github.com/airtai/cookiecutter-fastagency.git app_type=nats+fastapi+mesop && \
-    cd nats_n_fastapi && tree my_fastagency_app | sed 's/^/        /' > folder_structure.txt && cd .. && \
+    cd nats_n_fastapi && tree my_fastagency_app > folder_structure.txt && cd .. && \
     cd ../../..
 
 
