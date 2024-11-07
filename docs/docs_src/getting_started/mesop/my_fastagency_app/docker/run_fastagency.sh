cc5e29d3aab2cb2153334cb17e09959f744707ed
Tvrtko Sternak <117077296+sternakt@users.noreply.github.com>
2024-11-07 11:47:48 +0100

    Skip whatsapp agent e2e tests, add whatsapp unit tests (#540)
    * Skip whatsapp agent e2e tests, add whatsapp unit tests
    
    * Rebuild docs

--- docs/docs_src/getting_started/mesop/my_fastagency_app/docker/run_fastagency.sh
+++ docs/docs_src/getting_started/mesop/my_fastagency_app/docker/run_fastagency.sh
@@ -3,14 +3,35 @@
 # Accept env variable for PORT
 
 
-
-MESOP_PORT=${MESOP_PORT:-8888}
+export MESOP_PORT=${MESOP_PORT:-8888}
 
 # Default number of workers if not set
 WORKERS=${WORKERS:-1}
+echo "Number of workers: $WORKERS"
+
+# Generate nginx config
+for ((i=1; i<$WORKERS+1; i++))
+do
+	PORT=$((MESOP_PORT + i))
+    sed -i "5i\    server 127.0.0.1:$PORT;" nginx.conf.template
+done
+envsubst '${MESOP_PORT}' < nginx.conf.template >/etc/nginx/conf.d/default.conf
+cat /etc/nginx/conf.d/default.conf
+
+# Start nginx
+nginx -g "daemon off;" &
 
 # Run uvicorn server
 uvicorn my_fastagency_app.deployment.main_:app --host 0.0.0.0 --port $FASTAPI_PORT > /dev/stdout 2>&1 &
 
 # Run gunicorn server
-gunicorn --workers=$WORKERS my_fastagency_app.deployment.main:app --bind 0.0.0.0:$MESOP_PORT
+# Start multiple single-worker gunicorn instances on consecutive ports
+for ((i=1; i<$WORKERS+1; i++))
+do
+	PORT=$((MESOP_PORT + i))
+    echo "Starting gunicorn on port $PORT"
+    gunicorn --workers=1 my_fastagency_app.deployment.main:app --bind 0.0.0.0:$PORT > /dev/stdout 2>&1 &
+done
+
+# Wait for all background processes
+wait
--- docs/docs_src/getting_started/nats_n_fastapi/my_fastagency_app/docker/run_fastagency.sh
+++ docs/docs_src/getting_started/nats_n_fastapi/my_fastagency_app/docker/run_fastagency.sh
@@ -7,11 +7,23 @@ NATS_FASTAPI_PORT=${NATS_FASTAPI_PORT:-8000}
 
 FASTAPI_PORT=${FASTAPI_PORT:-8008}
 
-
-MESOP_PORT=${MESOP_PORT:-8888}
+export MESOP_PORT=${MESOP_PORT:-8888}
 
 # Default number of workers if not set
 WORKERS=${WORKERS:-1}
+echo "Number of workers: $WORKERS"
+
+# Generate nginx config
+for ((i=1; i<$WORKERS+1; i++))
+do
+	PORT=$((MESOP_PORT + i))
+    sed -i "5i\    server 127.0.0.1:$PORT;" nginx.conf.template
+done
+envsubst '${MESOP_PORT}' < nginx.conf.template >/etc/nginx/conf.d/default.conf
+cat /etc/nginx/conf.d/default.conf
+
+# Start nginx
+nginx -g "daemon off;" &
 
 # Run nats uvicorn server
 uvicorn my_fastagency_app.deployment.main_1_nats:app --host 0.0.0.0 --port $NATS_FASTAPI_PORT > /dev/stdout 2>&1 &
@@ -20,4 +32,13 @@ uvicorn my_fastagency_app.deployment.main_1_nats:app --host 0.0.0.0 --port $NATS
 uvicorn my_fastagency_app.deployment.main_2_fastapi:app --host 0.0.0.0 --port $FASTAPI_PORT > /dev/stdout 2>&1 &
 
 # Run gunicorn server
-gunicorn --workers=$WORKERS my_fastagency_app.deployment.main_3_mesop:app --bind 0.0.0.0:$MESOP_PORT
+# Start multiple single-worker gunicorn instances on consecutive ports
+for ((i=1; i<$WORKERS+1; i++))
+do
+	PORT=$((MESOP_PORT + i))
+    echo "Starting gunicorn on port $PORT"
+    gunicorn --workers=1 my_fastagency_app.deployment.main_3_mesop:app --bind 0.0.0.0:$PORT > /dev/stdout 2>&1 &
+done
+
+# Wait for all background processes
+wait
