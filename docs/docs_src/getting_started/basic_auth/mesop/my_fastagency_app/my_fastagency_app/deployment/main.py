6136a40a746949af5b76667e371240b81307eab5
Kumaran Rajendhiran <kumaran@airt.ai>
2024-11-13 12:05:17 +0000

    Update cookiecutter generated files

--- docs/docs_src/getting_started/basic_auth/mesop/my_fastagency_app/my_fastagency_app/deployment/main.py
+++ docs/docs_src/getting_started/basic_auth/mesop/my_fastagency_app/my_fastagency_app/deployment/main.py
@@ -1,11 +1,26 @@
 from fastagency import FastAgency
 from fastagency.ui.mesop import MesopUI
+from fastagency.ui.mesop.auth.basic_auth import BasicAuth
 
 from ..workflow import wf
 
+auth = BasicAuth(
+    # TODO: Replace `allowed_users` with the desired usernames and their
+    # bcrypt-hashed passwords. One way to generate bcrypt-hashed passwords
+    # is by using online tools such as https://bcrypt.online
+    # Default password for all users is `password`
+    allowed_users={
+        "admin": "$2y$10$ZgcGQlsvMoMRmmW4Y.nUVuVHc.vOJsOA7iXAPXWPFy9DX2S7oeTDa",  # nosemgrep: generic.secrets.security.detected-bcrypt-hash.detected-bcrypt-hash
+        "user@example.com": "$2y$10$ZgcGQlsvMoMRmmW4Y.nUVuVHc.vOJsOA7iXAPXWPFy9DX2S7oeTDa",  # nosemgrep: generic.secrets.security.detected-bcrypt-hash.detected-bcrypt-hash
+    },
+)
+
+ui = MesopUI(auth=auth)
+
+
 app = FastAgency(
     provider=wf,
-    ui=MesopUI(),
+    ui=ui,
     title="My FastAgency App",
 )
 
