2fb622daf8a3ce72f0f8edaeb4ee39cddb2c3c22
Kumaran Rajendhiran <kumaran@airt.ai>
2024-11-21 09:25:44 +0000

    Update cookiecutter generated files

--- docs/docs_src/getting_started/basic_auth/fastapi/my_fastagency_app/docker/content/run_fastagency.sh
+++ docs/docs_src/getting_started/basic_auth/fastapi/my_fastagency_app/docker/content/run_fastagency.sh
@@ -5,7 +5,9 @@
 
 FASTAPI_PORT=${FASTAPI_PORT:-8008}
 
+
 export MESOP_PORT=${MESOP_PORT:-8888}
+export SERVICE_PORT=$MESOP_PORT
 
 # Default number of workers if not set
 WORKERS=${WORKERS:-1}
@@ -18,24 +20,26 @@ echo "Fly machine ID: $FLY_MACHINE_ID"
 # Generate nginx config
 for ((i=1; i<$WORKERS+1; i++))
 do
-	PORT=$((MESOP_PORT + i))
+	PORT=$((SERVICE_PORT + i))
     sed -i "5i\    server 127.0.0.1:$PORT;" nginx.conf.template
 done
-envsubst '${MESOP_PORT},${FLY_MACHINE_ID}' < nginx.conf.template >/etc/nginx/conf.d/default.conf
+envsubst '${SERVICE_PORT},${FLY_MACHINE_ID}' < nginx.conf.template >/etc/nginx/conf.d/default.conf
 echo "Nginx config:"
 cat /etc/nginx/conf.d/default.conf
 
 # Start nginx
 nginx -g "daemon off;" &
 
+
 # Run uvicorn server
 uvicorn my_fastagency_app.deployment.main_1_fastapi:app --host 0.0.0.0 --port $FASTAPI_PORT > /dev/stdout 2>&1 &
+
 
 # Run gunicorn server
 # Start multiple single-worker gunicorn instances on consecutive ports
 for ((i=1; i<$WORKERS+1; i++))
 do
-	PORT=$((MESOP_PORT + i))
+	PORT=$((SERVICE_PORT + i))
     echo "Starting gunicorn on port $PORT"
     gunicorn --workers=1 my_fastagency_app.deployment.main_2_mesop:app --bind 0.0.0.0:$PORT > /dev/stdout 2>&1 &
 done
--- docs/docs_src/getting_started/no_auth/nats_n_fastapi/my_fastagency_app/docker/content/run_fastagency.sh
+++ docs/docs_src/getting_started/no_auth/nats_n_fastapi/my_fastagency_app/docker/content/run_fastagency.sh
@@ -7,7 +7,9 @@ NATS_FASTAPI_PORT=${NATS_FASTAPI_PORT:-8000}
 
 FASTAPI_PORT=${FASTAPI_PORT:-8008}
 
+
 export MESOP_PORT=${MESOP_PORT:-8888}
+export SERVICE_PORT=$MESOP_PORT
 
 # Default number of workers if not set
 WORKERS=${WORKERS:-1}
@@ -20,10 +22,10 @@ echo "Fly machine ID: $FLY_MACHINE_ID"
 # Generate nginx config
 for ((i=1; i<$WORKERS+1; i++))
 do
-	PORT=$((MESOP_PORT + i))
+	PORT=$((SERVICE_PORT + i))
     sed -i "5i\    server 127.0.0.1:$PORT;" nginx.conf.template
 done
-envsubst '${MESOP_PORT},${FLY_MACHINE_ID}' < nginx.conf.template >/etc/nginx/conf.d/default.conf
+envsubst '${SERVICE_PORT},${FLY_MACHINE_ID}' < nginx.conf.template >/etc/nginx/conf.d/default.conf
 echo "Nginx config:"
 cat /etc/nginx/conf.d/default.conf
 
@@ -33,14 +35,16 @@ nginx -g "daemon off;" &
 # Run nats uvicorn server
 uvicorn my_fastagency_app.deployment.main_1_nats:app --host 0.0.0.0 --port $NATS_FASTAPI_PORT > /dev/stdout 2>&1 &
 
+
 # Run uvicorn server
 uvicorn my_fastagency_app.deployment.main_2_fastapi:app --host 0.0.0.0 --port $FASTAPI_PORT > /dev/stdout 2>&1 &
+
 
 # Run gunicorn server
 # Start multiple single-worker gunicorn instances on consecutive ports
 for ((i=1; i<$WORKERS+1; i++))
 do
-	PORT=$((MESOP_PORT + i))
+	PORT=$((SERVICE_PORT + i))
     echo "Starting gunicorn on port $PORT"
     gunicorn --workers=1 my_fastagency_app.deployment.main_3_mesop:app --bind 0.0.0.0:$PORT > /dev/stdout 2>&1 &
 done
